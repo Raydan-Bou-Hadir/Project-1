e0403d195840fd63a20e92257ce20c596997c4ce
Raybh <raybouhadir67@gmail.com>
2021-05-08 22:13:30 +0300

    step-1

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import './scss/style.scss';
+import './css/style.css';
+import 'jquery/dist/jquery';
+import 'popper.js/dist/popper';
+import 'bootstrap/dist/js/bootstrap.min.js';
